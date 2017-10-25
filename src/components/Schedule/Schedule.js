8691fc6d0bec6ab5518188cb2f3c7c620d14c58e
sdinay <sdinay@yahoo.com>
2017-10-24 20:27:39 -0700

    working on schedule component

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import './Schedule.css';
 
 
 class Schedule extends Component{
     render(){
         return(
-            <div> Schedule Component, To be filled</div> 
-        ); 
+            <div>Schedule Component, To be filled</div>
+        );
     }
 }
 
