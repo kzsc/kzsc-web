edfc738b05553ca8dd5ce6ac3e9a30bf0c9458c5
Dylan Max Fabris <dfabris@ucsc.edu>
2017-12-04 20:53:31 -0800

    blog updates from wordpress

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,211 +1,69 @@
 import React, { Component } from 'react';
 import { Container, Button, Form, Grid, Column } from 'semantic-ui-react';
-import './Blog.css';
-
+import axios from 'axios';
+import ReactDOM from 'react-dom';
 import imgDefault from './kzsc.jpg';
+<<<<<<< HEAD
+
+
+
+
+=======
 var i = Math.floor(Math.random() * 93) + 1;
+>>>>>>> 3bf4596688208f40a2bee95c902b8663b3062d64
 
 class Blog extends Component {
     constructor(props) {
         super(props);
+<<<<<<< HEAD
+        this.state = {
+          allposts: []
+        };
     }
 
+    componentDidMount() {
+      axios.get(`https://www.kzsc.org/api/get_recent_posts/`)
+        .then(res => {
+          console.log(res);
+          const allposts = res.data.posts.map(obj => obj);
+          this.setState({ allposts });
+        });
+    }
+
+=======
+    }
+
+>>>>>>> 3bf4596688208f40a2bee95c902b8663b3062d64
     blogContent() {
-      i = (i+7)%93;
-        return (
-            <div className="div-blog">
-                <div class = "ui stackable four column grid">
-                        <div class="column">
-                            <div className="blogimage-container">
-                                <img className="blog-images" src={"https://www.kzsc.org/wp-content/uploads/2017/10/DB_Day-2-Katie-Laskowska-Drop-1-10-1030x686.jpg"} />
-                                {/*<div className="div-beneath-img">
-
-                                        <Button color="red"> Add </Button>
-                                    </div>*/}
-                            </div>
-                            <div className="blogtext-container">
-
-                              <h3>Dirtybird Campout: Summer Camp in October	</h3>
-                              <h5>by KZSC Web Team | 10.11.2017</h5>
-
-                              <p> Miss the days of tie dying, tug of rope, color wars,
-                              and the anticipated talent show? Dirtybird Campout was all that and more.
-                              </p>
-
-                              <a class="ui orange label">Festival</a>
-                              <a class="ui yellow label">RPM</a>
-                              <a class="ui green label">Events</a>
-
-
-
-
-                            </div>
-
-
-                        </div>
-                        <div class="column">
-                            <div className="blogimage-container">
-                                <img className="blog-images" src={"https://www.kzsc.org/wp-content/uploads/2017/10/Screen-Shot-2017-10-26-at-19.59.21.png"} />
-                                {/*<div className="div-beneath-img">
-
-                                        <Button color="red"> Add </Button>
-                                    </div>*/}
-                            </div>
-                            <div className="blogtext-container">
-
-                              <h3>BigMoney Lil Sheik interview w/ RIZ aka RSD 7.30.17</h3>
-                              <h5>by Rizal Aliga | 9.29.2017</h5>
-
-                              <p> I caught up with Lil Sheik of South Richmond,
-                               CA, during his YWN tour stop in Santa Cruz, CA with SOB X RBE.
-                              </p>
-
-                              <a class="ui orange label">Main</a>
-
-                            </div>
-
-
-                        </div>
-                        <div class="column">
-                            <div className="blogimage-container">
-                                <img className="blog-images" src={"https://www.kzsc.org/wp-content/uploads/2017/09/Screen-Shot-2017-09-29-at-16.08.55.png"} />
-                                {/*<div className="div-beneath-img">
-
-                                        <Button color="red"> Add </Button>
-                                    </div>*/}
-                            </div>
-                            <div className="blogtext-container">
-
-                              <h3>DJ Shellheart interview w/ RIZ aka RSD @ 6th Annual Hiero Day 2017 (9.4.17)</h3>
-                              <h5>by Rizal Aliga | 9.28.2017</h5>
-
-                              <p> I caught up with DJ Shellheart during Hiero Day 2017!
-                              </p>
-
-                              <a class="ui orange label">Main</a>
-
-                            </div>
-
-
-                        </div>
-                        <div class="column">
-                            <div className="blogimage-container">
-                                <img className="blog-images" src={"https://lh3.googleusercontent.com/--T9gVhe7vcc/Wfa4gVs-5fI/AAAAAAAAgu0/aiF0sWcoG80zlfhSL6HWCXEAx9NWSoZSgCHMYCw/s0/2017-10-29_22-28-30.png"} />
-                                {/*<div className="div-beneath-img">
-
-                                        <Button color="red"> Add </Button>
-                                    </div>*/}
-                            </div>
-                            <div className="blogtext-container">
-
-                              <h3>INTERVIEWS: LIFE OUT OF BALANCE – Belgian band We Stood Like Kings Releases New Koyaanisqatsi Soundtrack</h3>
-                              <h5>by John Malkin | 9.27.2017</h5>
-
-                              <p>  In 1982 the film Koyaanisqatsi had its first
-                               public screening and quickly became an influential classic.
-                              </p>
-
-                              <a class="ui orange label">Main</a>
-
-                            </div>
-
-
-                        </div>
-                        <div class="column">
-                            <div className="blogimage-container">
-                                <img className="blog-images" src={"https://www.kzsc.org/wp-content/uploads/2017/10/Screen-Shot-2017-10-26-at-19.59.21.png"} />
-                                {/*<div className="div-beneath-img">
-
-                                        <Button color="red"> Add </Button>
-                                    </div>*/}
-                            </div>
-                            <div className="blogtext-container">
-
-                              <h3>BigMoney Lil Sheik interview w/ RIZ aka RSD 7.30.17</h3>
-                              <h5>by Rizal Aliga</h5>
-
-                              <p> I caught up with Lil Sheik of South Richmond,
-                               CA, during his YWN tour stop in Santa Cruz, CA with SOB X RBE.
-                              </p>
-
-                              <a class="ui orange label">Main</a>
-
-                            </div>
-
-
-                        </div>
-                        <div class="column">
-                            <div className="blogimage-container">
-                                <img className="blog-images" src={"https://www.kzsc.org/wp-content/uploads/2017/10/Screen-Shot-2017-10-26-at-19.59.21.png"} />
-                                {/*<div className="div-beneath-img">
-
-                                        <Button color="red"> Add </Button>
-                                    </div>*/}
-                            </div>
-                            <div className="blogtext-container">
-
-                              <h3>BigMoney Lil Sheik interview w/ RIZ aka RSD 7.30.17</h3>
-                              <h5>by Rizal Aliga</h5>
-
-                              <p> I caught up with Lil Sheik of South Richmond,
-                               CA, during his YWN tour stop in Santa Cruz, CA with SOB X RBE.
-                              </p>
-
-                              <a class="ui orange label">Main</a>
-
-                            </div>
-
-
-                        </div>
-                        <div class="column">
-                            <div className="blogimage-container">
-                                <img className="blog-images" src={require('./albumart/' + (((i+2)%93)+1) + '.jpg')} />
-                                {/*<div className="div-beneath-img">
-                                        <Button color="red"> Add </Button>
-                                    </div>*/}
-                            </div>
-                            <div className="blogtext-container">
-
-                            </div>
-                        </div>
-                        <div class="column">
-                            <div className="blogimage-container">
-                                <img className="blog-images" src={require('./albumart/' + (((i+3)%93)+1) + '.jpg')} />
-                                {/*<div className="div-beneath-img">
-                                        <Button color="red"> Add </Button>
-                                    </div>*/}
-                            </div>
-                            <div className="blogtext-container">
-
-                            </div>
-                        </div>
-                        <div class="column">
-                            <div className="blogimage-container">
-                                <img className="blog-images" src={require('./albumart/' + (((i+4)%93)+1) + '.jpg')} />
-                                {/*<div className="div-beneath-img">
-                                        <Button color="red"> Add </Button>
-                                    </div>*/}
-                            </div>
-                            <div className="blogtext-container">
-
-
-                            </div>
-                        </div>
-                        <div class="column">
-                            <div className="blogimage-container">
-                                <img className="blog-images" src={require('./albumart/' + (((i+5)%93)+1) + '.jpg')} />
-                                {/*<div className="div-beneath-img">
-                                        <Button color="red"> Add </Button>
-                                    </div>*/}
-                            </div>
-                            <div className="blogtext-container">
-
-
-                            </div>
-                        </div>
+      return (
+          <div class="ui four link cards">
+            {this.state.allposts.map(curpost =>
+              <div class="card">
+                <div class="image">
+                  <img src={curpost.thumbnail_images.full.url}/>
                 </div>
-            </div>
-        );
+                <div class="content">
+                  <div class="header" key={curpost.id}>{curpost.title}</div>
+                  <div class="meta">
+                    <a>By {curpost.author.name}</a>
+                  </div>
+                  <div class="description">
+                    {curpost.excerpt}
+                  </div>
+                </div>
+                <div class="extra content">
+                  <span class="right floated">
+                    {curpost.date}
+                  </span>
+                  <span>
+                    <i class="tags icon"></i>
+
+                  </span>
+                </div>
+                </div>
+            )}
+          </div>
+      );
     }
 
     render() {
