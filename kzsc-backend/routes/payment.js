5910123945ac118c63845f46e73321203fd792db
ogparra <ogparra.15@gmail.com>
2017-12-12 13:46:36 -0800

    Updated donate page

--- kzsc-backend/routes/payment.js
+++ kzsc-backend/routes/payment.js
@@ -2,21 +2,23 @@ var express = require('express');
 var router = express.Router();
 var stripe = require("stripe")("");
 
-router.get('/', function(req, res, next){
-    console.log("Inside Payment method");
-    res.send("Data was returned back")
-    // var token = request.body.stripeToken;
-    
-    // stripe.charges.create({
-    //     amount: 1,
-    //     currency: "usd",
-    //     description: "Example charge",
-    //     source: token,
-    // }, function(err, charge) {
-    //     console.log("Callback function called");
-    //     console.log(Charge); 
-    //     // asynchronously called
-    // });
+// Using token returned from Stripe process transaction
+router.post('/', function(req, res, next){
+    var tokenid = req.body.id;
+
+    stripe.charges.create({
+        amount: 1000,
+        currency: "usd",
+        description: "Posting transaction to the card",
+        source: tokenid,
+    }, function(err, charge) {
+        if(err){ 
+            console.log("There was an error making the charge");
+            console.log(err.message);
+        }else{
+            res.send('Transaction has been processed');
+        }
+    });
 });
 
 module.exports = router;
