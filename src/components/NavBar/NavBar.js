9b167314b2b539664b365427d21a512f37d44054
sdinay <sdinay@yahoo.com>
2017-12-12 13:59:24 -0800

    fixed play button loading

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,18 +6,24 @@ import NavBarMobile from './NavBarMobile';
 class Home extends Component{
     render(){
         return(
-          <Grid>
-            <Grid.Row columns={1} only='mobile'>
-              <Grid.Column>
-                <NavBarMobile></NavBarMobile>
-              </Grid.Column>
-            </Grid.Row>
-            <Grid.Row columns={1} only='tablet computer'>
-              <Grid.Column>
-                <NavBarFull></NavBarFull>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+          <div>
+            <Grid>
+              <Grid.Row columns={1} only='mobile'>
+                <Grid.Column>
+                  <NavBarMobile></NavBarMobile>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row columns={1} only='tablet computer'>
+                <Grid.Column>
+                  <NavBarFull></NavBarFull>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+            <audio id="player" preload="auto">>
+              <source src="http://188.165.192.5:8242/kzschigh?type=.mp3"
+              type="audio/mpeg" preload="auto"/>
+            </audio>
+          </div>
         );
     }
 }
