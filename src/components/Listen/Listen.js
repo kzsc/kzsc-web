143c702f524018294efab63e9f0d9aa8c4e2d37d
ogparra <ogparra.15@gmail.com>
2017-12-12 15:50:24 -0800

    Modified Listen Page

--- src/components/Listen/Listen.js
+++ src/components/Listen/Listen.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Grid, Table } from 'semantic-ui-react';
 import FaApple from 'react-icons/lib/fa/apple';
 import FaSoundcloud from 'react-icons/lib/fa/soundcloud';
+import FaAndroid from 'react-icons/lib/fa/android';
 import './Listen.css';
 
 class Listen extends Component{
@@ -12,9 +13,10 @@ class Listen extends Component{
                 <Grid.Row className="list-row listen">
                     <Grid.Column  size={16}>
                         <div className="table-desc">
-                        KZSC doesn’t just broadcast to 3 million people over the radio airwaves at 88.1FM, we also broadcast to the entire world over the internet tubes! This table below explains how you can tune in, even when you’re not in lovely Santa Cruz.
-                        <br/>
-                        For our main stream, you can use one of the two different quality webstreams listed below, find us in iTunes under “College Radio”, or just press the red PLAY button to the right.
+                            <h1> Listen to KZSC 88.1 FM </h1>
+                            <p>KZSC doesn’t just broadcast to 3 million people over the radio airwaves at 88.1FM, we also broadcast to the entire world over the internet tubes! This table below explains how you can tune in, even when you’re not in lovely Santa Cruz.</p>
+                            <h2> KZSC Main Stream </h2>
+                            <p>You can use one of the two different quality webstreams listed below, for iTunes search under “College Radio”, or just press the red PLAY button to the right.</p>
                         </div>
                         <div className="table1">
                             <Table size="large">
@@ -61,16 +63,16 @@ class Listen extends Component{
                         </div>
                     </Grid.Column>
                 </Grid.Row>
+                <h2> Also, listen to KZSC 88.1FM on Android and iOS </h2>
+                <p className="table-desc"> On the iPhone, KZSC Radio is simple and easy to use – one click and you’re listening! You’ll have access to the main station, our new secondary webstream, and current playlist information. </p>
                 <Grid.Row className="list-row listen">
                     <Grid.Column  size={16}>
-                    <div className="mobile-desc"> Listen to KZSC 88.1FM on Android and iOS </div>
-                    <a href="https://play.google.com/store/apps/details?id=org.kzsc.streaming">
-                    </a>
-                    <a href="https://itunes.apple.com/us/app/kzsc-radio/id371572770?mt=8#"><FaApple size={100} className="fa-apple"/></a>
+                        <a href="https://play.google.com/store/apps/details?id=org.kzsc.streaming"><FaAndroid size={150} className="fa-android"/></a>
+                        <a href="https://itunes.apple.com/us/app/kzsc-radio/id371572770?mt=8#"><FaApple size={150} className="fa-apple"/></a>
                     </Grid.Column>
                 </Grid.Row>
-                <Grid.Row className="list-row listen">
-                    <Grid.Column color="green" size={16}>
+                <Grid.Row className="list-row listen2">
+                    <Grid.Column color="white" size={16}>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
@@ -78,7 +80,7 @@ class Listen extends Component{
         );
     }
 }
-{/* https://itunes.apple.com/us/app/kzsc-radio/id371572770?mt=8# */ }
 
+// The KZSC Radio app is a free Internet radio streaming service tuned to community radio station 88.1 FM KZSC, featuring award-winning music programming, UCSC sports, local news and stimulating conversation.
 
 export default Listen;
