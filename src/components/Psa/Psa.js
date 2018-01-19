c6c1258ea3cad06fb3a0e8bf79ec6a14e44aa593
sdinay <sdinay@yahoo.com>
2018-01-19 00:51:38 -0800

    made progress on about contact cards

---
+++ src/components/Psa/Psa.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react'
+import uuid from 'uuid';
+
+class Psa extends Component{
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="Psa"></div>
+    );
+  }
+
+}
+
+export default Psa;
