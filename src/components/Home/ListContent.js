4a87e75aa4bc16bdae409b63ed528fbf1d1cf09c
sdinay <sdinay@yahoo.com>
2018-01-19 00:07:32 -0800

    still working on about dimmer

--- src/components/Home/ListContent.js
+++ src/components/Home/ListContent.js
@@ -3,63 +3,63 @@ import { Grid } from 'semantic-ui-react';
 import './ListContent.css';
 
 class ListContent extends Component{
-    render(){
-        return(
-            <div className="list-content">
-                <h2> Interviews </h2>
-                <Grid>
-                    <Grid.Row className="list-row">
-                        <Grid.Column width={4} color="red" className="grid-item">
-                            Content 1
-                        </Grid.Column>
-                        <Grid.Column width={4} color="green" className="grid-item">
-                            Content 2
-                        </Grid.Column>
-                        <Grid.Column width={4} color="yellow" className="grid-item">
-                            Content 3
-                        </Grid.Column>
-                        <Grid.Column width={4} color="grey" className="grid-item">
-                            Content 4
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-                <h2> Events </h2>
-                <Grid>
-                    <Grid.Row className="list-row">
-                        <Grid.Column width={4} color="red" className="grid-item">
-                            Content 1
-                        </Grid.Column>
-                        <Grid.Column width={4} color="green" className="grid-item">
-                            Content 2
-                        </Grid.Column>
-                        <Grid.Column width={4} color="yellow" className="grid-item">
-                            Content 3
-                        </Grid.Column>
-                        <Grid.Column width={4} color="grey" className="grid-item">
-                            Content 4
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-                <h2> Blog </h2>
-                <Grid>
-                    <Grid.Row className="list-row">
-                        <Grid.Column width={4} color="red" className="grid-item">
-                            Content 1
-                        </Grid.Column>
-                        <Grid.Column width={4} color="green" className="grid-item">
-                            Content 2
-                        </Grid.Column>
-                        <Grid.Column width={4} color="yellow" className="grid-item">
-                            Content 3
-                        </Grid.Column>
-                        <Grid.Column width={4} color="grey" className="grid-item">
-                            Content 4
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </div>
-        );
-    }
+  render(){
+    return(
+      <div className="list-content">
+        <h2>Interviews</h2>
+        <Grid>
+          <Grid.Row className="list-row">
+            <Grid.Column width={4} color="red" className="grid-item">
+              Content 1
+            </Grid.Column>
+            <Grid.Column width={4} color="green" className="grid-item">
+              Content 2
+            </Grid.Column>
+            <Grid.Column width={4} color="yellow" className="grid-item">
+              Content 3
+            </Grid.Column>
+            <Grid.Column width={4} color="grey" className="grid-item">
+              Content 4
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <h2> Events </h2>
+        <Grid>
+          <Grid.Row className="list-row">
+            <Grid.Column width={4} color="red" className="grid-item">
+              Content 1
+            </Grid.Column>
+            <Grid.Column width={4} color="green" className="grid-item">
+              Content 2
+            </Grid.Column>
+            <Grid.Column width={4} color="yellow" className="grid-item">
+              Content 3
+            </Grid.Column>
+            <Grid.Column width={4} color="grey" className="grid-item">
+              Content 4
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <h2> Blog </h2>
+        <Grid>
+          <Grid.Row className="list-row">
+            <Grid.Column width={4} color="red" className="grid-item">
+              Content 1
+            </Grid.Column>
+            <Grid.Column width={4} color="green" className="grid-item">
+              Content 2
+            </Grid.Column>
+            <Grid.Column width={4} color="yellow" className="grid-item">
+              Content 3
+            </Grid.Column>
+            <Grid.Column width={4} color="grey" className="grid-item">
+              Content 4
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 
